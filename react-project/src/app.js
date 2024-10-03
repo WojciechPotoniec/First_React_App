7d0a29ed87f81fde6a08e799e81dd0b83f1eba4d
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-10-03 17:16:20 +0200

    some snacks to refresh js syntax and various method

---
+++ react-project/src/app.js
@@ -0,0 +1,20 @@
+export function combine(a, b, c) {
+    let sum = a * b / c;
+    return sum;
+}
+console.log(combine(10, 3, 2))
+// expected output: 15
+
+const hobbies = ['sports', 'cooking', 'music']
+const editedHobbies = hobbies.map((item) => ({item: item}))
+console.log(editedHobbies);
+// expected output: [{item: 'sports'}, {item: 'cooking'}, {item: 'music'}]
+
+function transformToObjects(numberArray) {
+    const numberObject = numberArray.map((number) => ({value: number}));
+    return numberObject;
+ }
+
+ const array = [1, 2, 3];
+ console.log(transformToObjects(array));
+ // expected output: [{value: 1}, {value: 2}, {value: 3}]
