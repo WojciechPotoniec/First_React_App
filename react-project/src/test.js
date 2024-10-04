d993abfaf92f5c88091e15e9e0b02c5b8b742418
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-10-04 13:09:52 +0200

    reduce methods snack

--- react-project/src/test.js
+++ react-project/src/test.js
@@ -40,7 +40,8 @@ const names = ['mario', 'luigi', 'peach', 'bowser'];
 
 const capitalizedNames = names.map((name) => name.charAt().toUpperCase() + name.slice(1));
 console.log(capitalizedNames);
-
+// Expected output dato l'array di stringhe ho combinato diversi metodi per poter rimappare e creare un nuovo array di nomi con le lettere maiuscole.
+// .map rimappa e crea un nuovo array | .charAt recupera di default il primo elemento e lo restituisce in questo caso le lettere 'm','l','p','b' | toUpperCase rende maiuscoli questi elementi | e aggiungendo name.slice partendo dalla posizione numero 1 aggiunge il resto dei caratteri mancanti in questo caso.
 
 // find() - Trovare un elemento
 // Esercizio: Dato un array di oggetti che rappresentano prodotti, trova il prodotto con il prezzo uguale a 100.
@@ -54,6 +55,8 @@ const products = [
   const findedProduct = products.find((product) => product.price === 100);
 
   console.log(findedProduct);
+// Expected output object at the 2 index position, perchè il metodo find restituisce da un array di obj il prodotto che rispetta la condizione posta sul prezzo.
+
 
 // findIndex() - Trovare l'indice di un elemento
 // Esercizio: Dato un array di numeri, trova l'indice del primo numero pari.
@@ -63,3 +66,14 @@ const numbers = [5, 3, 7, 6, 8];
 const evenNumber = numbers.findIndex((number) => number % 2 === 0);
 
 console.log(evenNumber);
+// Expected output è 3 perchè trova l'indice del numero all'interno dell'array che diviso per 2 porta come risultato 0 quindi risulta un numero pari.
+
+// reduce() - Accumulare i valori
+// Esercizio: Dato un array di numeri, trova il prodotto di tutti i numeri (moltiplicali tra di loro).
+
+const arrayNumbers = [2, 3, 4, 6];
+
+const productOfNumber = arrayNumbers.reduce((previousValue, currentNumber) => previousValue * currentNumber);
+
+console.log(productOfNumber);
+// Expected output è 144 perchè la moltiplicazione avviene partendo dal primo elemento finche non finiscono. in questo caso senza valore iniziale passato alla fine perchè si tratta di una moltiplicazione altrimenti avremmo passato lo 0
